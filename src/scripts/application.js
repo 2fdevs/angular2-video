9fec92ec4cf2f2bce9038666002dea953e9bd0bd
Raúl Jiménez <raul@fccma.com>
2015-04-04 00:27:41 +0200

    First commit.

---
+++ src/scripts/application.js
@@ -0,0 +1,21 @@
+import {Component, Template} from 'angular2/angular2';
+import {VideogularPlayer} from 'scripts/com/2fdevs/videogular/core/vg-player';
+
+@Component({
+    selector: 'application'
+})
+@Template({
+    url: 'scripts/application.html',
+    directives: [VideogularPlayer]
+})
+export class Application {
+    source: Array;
+
+    constructor() {
+        this.source = [
+            {src: 'http://static.videogular.com/assets/videos/videogular.mp4', type: 'video/mp4'},
+            {src: 'http://static.videogular.com/assets/videos/videogular.webm', type: 'video/webm'},
+            {src: 'http://static.videogular.com/assets/videos/videogular.ogg', type: 'video/ogg'}
+        ];
+    }
+}
---
+++ src/scripts/com/2fdevs/videogular/core/vg-player.js
@@ -0,0 +1,28 @@
+import {Component, Template, For} from 'angular2/angular2';
+
+@Component({
+    selector: 'vg-player',
+    bind: {
+        'src': 'src'
+    }
+})
+@Template({
+    url: 'scripts/com/2fdevs/videogular/core/vg-player.html',
+    directives: [For]
+})
+export class VideogularPlayer {
+    src:String;
+    isPlaying:Boolean = false;
+    element:HTMLElement;
+
+    constructor() {
+        this.element = document.querySelector("vg-player video");
+    }
+
+    playPause() {
+        this.isPlaying = !this.isPlaying;
+
+        if (this.isPlaying) this.element.play();
+        else this.element.pause();
+    }
+}
